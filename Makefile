ba87c0af319224ca9ceb9d87a7465321f8de892d
Asayu Yuki <asayu.yuki@gmail.com>
2021-05-23 18:53:11 +0200

    Added licence notice in source files

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ CFLAGS += -c -Wall -std=c89 -pedantic-errors -g
 LDFLAGS = $$(sdl2-config --libs) $$(xml2-config --libs) -lSDL2_image -lSDL2_mixer -lm
 
 # Main target and filename of the executable
-OUT = AUDIRO
+OUT = PATHFIND
 
 # Build Directory
 BUILD_DIR = build
--- src/types.c
+++ src/types.c
@@ -1,3 +1,23 @@
+/*
+    PATHFIND is a simple game where the player aims to find chests either
+    by themself or by using the included pathfinding algorythm
+
+    Copyright (C) 2021 Killian RAIMBAUD (killian.rai@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 2 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #include "types.h"
 
 Vector2i InitVector2i(int x, int y){
